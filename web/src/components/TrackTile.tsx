3bb3306915f5d10dbae0b5406c014849d32effa5
rickyrombo <rickyrombo@users.noreply.github.com>
2025-11-30 19:55:37 -0800

    default to profile pic if no cover art

--- web/src/components/TrackTile.tsx
+++ web/src/components/TrackTile.tsx
@@ -471,6 +471,15 @@ function TrackTile({
             <CoverArt src={url} alt={track.title} onError={onError} />
           )}
         </WithMirrors>
+      ) : user?.profile_picture ? (
+        <WithMirrors
+          url={user.profile_picture.medium}
+          mirrors={user.profile_picture.mirrors}
+        >
+          {(url, onError) => (
+            <CoverArt src={url} alt={user.name} onError={onError} />
+          )}
+        </WithMirrors>
       ) : null}
       <TrackContent $isExpanded={isActive}>
         <TrackHeader>
